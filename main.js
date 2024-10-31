b904b1b5d331fb37043587cf19eadab132398122
hlungwani-al <hlungwani.al6@gmail.com>
2024-10-31 16:32:04 +0200

    register done... working on login

---
+++ main.js
@@ -0,0 +1,74 @@
+
+
+// Registering a user
+
+function registerUser(event){
+    event.preventDefault();
+
+    const inputUsername = document.getElementById('username');
+    const inputPassword = document.getElementById('password');
+    const inputConfirmPassword = document.getElementById('confirmPassword');
+
+    const username = inputUsername.value.trim();
+    const password = inputPassword.value;
+    const confirmPassword = inputConfirmPassword.value;
+
+    if (password !== confirmPassword){
+        alert("Passwords don't match!");
+        return;
+    }
+
+    if (username && password){
+        const storedUsers = JSON.parse(localStorage.getItem('users')) || [];
+        const userExist = storedUsers.some(user => user.usernames === username);
+        if(userExist){
+            alert("Username already exist");
+            return;
+        }
+        storedUsers.push({usernames: username, passwords:password});
+        localStorage.setItem('users', JSON.stringify(storedUsers));
+        alert("Registration successfull");
+
+        window.location.href = "success.html";
+        
+    } else {
+        alert("Username or Password not valid");
+    }
+}
+
+// user login
+function userLogin(event){
+    event.preventDefault();
+    const inputLoginUsername = document.getElementById('usernameLogin');
+    const inputLoginPassword = document.getElementById('passwordLogin');
+
+    const loginUsername = inputLoginUsername.value.trim();
+    const loginPassword = inputLoginPassword.value;
+
+    if (loginUsername){
+        if(loginPassword){
+            const storedUsers = JSON.parse(localStorage.getItem('users')) || [];
+            let userFound = 0;
+            if (storedUsers.length > 0){
+                storedUsers.forEach(user => {
+                    if(user.usernames === loginUsername && user.passwords === loginPassword){
+                        userFound = 1;
+                        alert("you are successfully loggenin");
+                        window.location.href = "success.html";
+                    }else
+                        alert("user not found");
+                });
+
+            }else{
+                alert("username doesn't exist, please register");
+                window.location.href = "register.html";
+            }
+
+        }else{
+            alert("Please enter a password")
+        }
+
+    } else {
+        alert("Please enter a username");
+    }
+}
